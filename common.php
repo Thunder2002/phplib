067e1ec31ce4bf1b7c5891f3a6d01d52cbf3cfdd
Christian Hunke <chris_hunke@web.de>
2013-11-20 17:25:28 +0100

    Basic object and string stuff

--- common.php
+++ common.php
@@ -1,3 +1,18 @@
 <?php
 
+function mb_strrev($str, $enc) {
+	if ($enc === null) {
+		$enc = mb_detect_encoding($str);
+	}
+
+	$len = mb_strlen($str, $enc);
+	$rev = '';
+
+	while ($len-- > 0) {
+		$rev .= mb_substr($mb_str, $len, 1, $enc);
+	}
+
+	return $rev;
+}
+
 ?>
