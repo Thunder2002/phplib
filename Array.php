dc6d52a055b521ab328f49eaa531ff5fbb264123
Christian Hunke <chris_hunke@web.de>
2013-11-26 19:17:48 +0100

    Add array construct

---
+++ Array.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace Thunder2002\PhpLib;
+
+require_once('Object.php');
+
+class Array extends Object implements \ArrayAccess {
+	public function __construct($array = null) {
+	}
+
+	public function clear() {
+	}
+
+	public function clone() {
+	}
+
+	public function convertAll() {
+	}
+
+	public function copy() {
+	}
+
+	public function exists($predicate) {
+	}
+
+	public function find($predicate) {
+	}
+
+	public function findAll($predicate) {
+	}
+
+	public function findIndex($predicate) {
+	}
+
+	public function findLast($predicate) {
+	}
+
+	public function findLastIndex($predicate) {
+	}
+
+	public function forEach($action) {
+	}
+
+	public function indexOf($item) {
+	}
+
+	public function lastIndexOf($item) {
+	}
+
+	public function reverse() {
+	}
+
+	public function sort($sortFunc) {
+	}
+
+	public function count() {
+	}
+
+	public function add($item) {
+	}
+
+	public function contains($item) {
+	}
+
+	public function insert($index, $item) {
+	}
+
+	public function remove($item) {
+	}
+
+	public function removeAt($index) {
+	}
+
+	public function all($predicate) {
+	}
+
+	public function any($predicate) {
+	}
+
+	public function concat($array) {
+	}
+
+	public function distinct() {
+	}
+
+	public function except($array) {
+	}
+
+	public function first($predicate = null) {
+	}
+
+	public function last($predicate = null) {
+	}
+
+	public function intersect($array) {
+	}
+
+	public function single($predicate = null) {
+	}
+
+	public function skip($count) {
+	}
+
+	public function take($count) {
+	}
+}
